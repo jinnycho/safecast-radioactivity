d7a4dd92034ca57c47492329e97600f0a96c4abe
Jinny Cho <45345735+jinnycho@users.noreply.github.com>
2019-07-13 17:00:40 -0400

    Clean up the data (#11)
    * Add build.sbt
    
    * Add project/assembly.sbt
    
    * Add src directory
    
    * Set up basic SafecastClustering.scala
    
    * Set up a basic code that cleans data
    
    * Update build.sbt that passes
    
    * Update name in build.sbt
    
    * Update SafecastClustering.scala to filter out header
    
    * Parse string to int
    
    * Use single quote, cancle Integer.parseInt
    
    * Make sure to convert to double since that's the accurate type
    
    * Add results that come after sbt assembly
    
    * Use spark dataframe so I can filter more easily
    
    * Update script to use dataframe & drop columns that are not necessary
    
    * update the script so it filters n/a + unnecessary columns + string values
    
    * Update all that is the result of sbt assembly
    
    * ----- 😘 empty commit -----
    
    * Create a function for better arrangement of the file
    
    * Add result of sbt assembly with this new function
    
    * Update comment
    
    * Update the result of sbt assembly after updating comments

---
+++ src/main/scala/SafecastClustering.scala
@@ -0,0 +1,65 @@
+package com.sundogsoftware.spark
+
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.log4j._
+import scala.io.Source
+import java.nio.charset.CodingErrorAction
+import scala.io.Codec
+import scala.math.sqrt
+import org.apache.spark.sql._
+import org.apache.spark.sql.functions._
+
+
+object SafecastClustering {
+
+  // Case class for a column name for lat, lon, value (radioactivity)
+  final case class SafecastR(lat: Double, lon: Double, value: Double)
+
+  /*
+   * Filter the given data
+   * Create a dataframe of latitude, longitude, value
+   */
+  def cleanData(safecastDF: DataFrame) : DataFrame = {
+    // filter unnecessary columns
+    val columnFilterlist = List("captured_at", "unit", "location_name", "device_id", "id", "user_id", "original_id", "measurement_import_id", "height", "devicetype_id", "sensor_id", "station_id", "channel_id")
+    var columnFilteredDF = safecastDF
+    for (col <- columnFilterlist) {
+        columnFilteredDF = columnFilteredDF.drop(col)
+    }
+    // filter null values
+    var nullFilteredDF = columnFilteredDF.na.drop()
+    // filter if it's not numeric
+    var stringFilterList = List("latitude", "longitude", "value")
+    var stringFilteredDF = nullFilteredDF
+    for (col <- stringFilterList) {
+        stringFilteredDF = stringFilteredDF.filter(row => row.getAs[String](col).matches("""^\d{1,}\.*\d*$"""))
+    }
+    return stringFilteredDF
+  }
+
+  /** Our main function where the action happens */
+  def main(args: Array[String]) {
+
+    // Set the log level to only print errors
+    Logger.getLogger("org").setLevel(Level.ERROR)
+
+    // Use new SparkSession interface in Spark 2.0
+    val spark= SparkSession
+      .builder
+      .appName("SafecastClustering")
+      .master("local[*]")
+      .getOrCreate()
+
+    val safecastDF = spark.read
+      .format("csv")
+      .option("header", "true") // filter header
+      .option("charset", "UTF8")
+      .load("/Users/jinnycho/Downloads/mini-measurements.csv")
+
+    val stringFilteredDF = cleanData(safecastDF)
+    stringFilteredDF.show()
+
+    spark.stop()
+  }
+}
