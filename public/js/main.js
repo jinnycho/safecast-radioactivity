51401dc18f3bca233b32547b04b77cc1d556b37c
jinnycho <jinnycho.eng@gmail.com>
2019-07-17 00:06:06 -0400

    Add public/js/main.js

---
+++ public/js/main.js
@@ -0,0 +1,9 @@
+/*
+ * This file is mainly used to
+ * 1. render the map
+ * 2. visualize data 
+ */
+
+const mapAccessToken = 'pk.eyJ1Ijoibm9yY2hhcmQiLCJhIjoiY2oyMHcxNXNhMDUwMTMzbnVkcmJ1eWszdSJ9.Dx5NmmL0h6xm3cUE5jLuJg'
+
+
