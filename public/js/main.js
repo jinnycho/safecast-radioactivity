5dc545e752d95153018c28dfee2ae2cb916a63c3
jinnycho <jinnycho.eng@gmail.com>
2019-07-19 03:28:38 -0400

    Not erroring and show dots in the beginning but disappear right after :/

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,7 @@
 'use strict';
 const width = 700;
 const height = 580;
+let view = "map";
 
 /*
  * This file is mainly used to
@@ -8,43 +9,53 @@ const height = 580;
  * 2. visualize data 
  */
 
-mapboxgl.accessToken = 'pk.eyJ1IjoiamlubnljaG81MDMiLCJhIjoiY2o2am16cnA5MDhxMTMycGR0MXRhaDZxNiJ9.lNDt1qFFi4V7zUin8Jj1LQ';
-var map = new mapboxgl.Map({
+d3.json('./data/clusters-1.geojson', (err, data) => {
+  drawMap(data);
+
+  function drawMap(data) {
+  mapboxgl.accessToken = 'pk.eyJ1IjoiamlubnljaG81MDMiLCJhIjoiY2o2am16cnA5MDhxMTMycGR0MXRhaDZxNiJ9.lNDt1qFFi4V7zUin8Jj1LQ';
+  var map = new mapboxgl.Map({
     container: 'map',
     style: 'mapbox://styles/mapbox/light-v10',
-    zoom: 12,
+    zoom: 1,
     center: [-122.447303, 37.753574]
-});
+  });
 
-// create SVG canvas
-let svg = d3.select("body")
+  var canvas = map.getCanvasContainer();
+
+  // create SVG canvas
+  var svg = d3.select(canvas)
     .append("svg")
-    .attr("width", width)
-    .attr("height", height);
+    .attr("width", "100%")
+    .attr("height", "100vh")
+    .attr("position", "absolute");
 
-// append empty placeholder g element to SVG
-// g will contain geometry elements
-let g = svg.append("g");
 
-// Width and Height of the whole visualization
-// Set Projection Parameters
-var albersProjection = d3.geoAlbers()
-    .scale( 190000 )
-    .rotate( [71.057,0] )
-    .center( [0, 42.313] )
-    .translate( [width/2,height/2] );
+  // Map d3 projection coordinates to Mapbox map coordinates 
+  function projectPoint(lon, lat) {
+    let point = map.project(new mapboxgl.LngLat(lon, lat));
+    this.stream.point(point.x, point.y);
+  }
+  var transform = d3.geoTransform({point: projectPoint});
 
-// turn lat/lon coordinates into screen coordinates
-var geoPath = d3.geoPath()
-    .projection(albersProjection);
+  // turn lat/lon coordinates into screen coordinates
+  var path = d3.geoPath()
+    .projection(transform);
 
-d3.json('./data/clusters-1.geojson', (err, data) => {
-  console.log(data);
-  g.selectAll("path")
+  var points = svg.selectAll("path")
     .data(data.features)
     .enter()
     .append("path")
+    .attr("d", path)
     .attr("fill", "#900")
-    .attr("stroke", "#999")
-    .attr("d", geoPath);
+    .attr("stroke", "#999");
+
+  }
+
+  function update() {
+    if (view === "map") {
+      drawMap(data);
+    }
+  }
+  update(500);
 });
