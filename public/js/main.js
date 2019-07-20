fa373afbd40199eefb869979725dedc016ac2a48
jinnycho <jinnycho.eng@gmail.com>
2019-07-20 04:33:46 -0400

    Update main.js and now circles rendered

--- public/js/main.js
+++ public/js/main.js
@@ -9,53 +9,56 @@ let view = "map";
  * 2. visualize data 
  */
 
-d3.json('./data/clusters-1.geojson', (err, data) => {
-  drawMap(data);
-
-  function drawMap(data) {
-  mapboxgl.accessToken = 'pk.eyJ1IjoiamlubnljaG81MDMiLCJhIjoiY2o2am16cnA5MDhxMTMycGR0MXRhaDZxNiJ9.lNDt1qFFi4V7zUin8Jj1LQ';
-  var map = new mapboxgl.Map({
-    container: 'map',
-    style: 'mapbox://styles/mapbox/light-v10',
-    zoom: 1,
-    center: [-122.447303, 37.753574]
-  });
-
-  var canvas = map.getCanvasContainer();
-
-  // create SVG canvas
-  var svg = d3.select(canvas)
-    .append("svg")
-    .attr("width", "100%")
-    .attr("height", "100vh")
-    .attr("position", "absolute");
-
-
-  // Map d3 projection coordinates to Mapbox map coordinates 
-  function projectPoint(lon, lat) {
-    let point = map.project(new mapboxgl.LngLat(lon, lat));
-    this.stream.point(point.x, point.y);
-  }
-  var transform = d3.geoTransform({point: projectPoint});
-
-  // turn lat/lon coordinates into screen coordinates
-  var path = d3.geoPath()
-    .projection(transform);
-
-  var points = svg.selectAll("path")
-    .data(data.features)
-    .enter()
-    .append("path")
-    .attr("d", path)
-    .attr("fill", "#900")
-    .attr("stroke", "#999");
-
-  }
-
-  function update() {
-    if (view === "map") {
-      drawMap(data);
-    }
-  }
-  update(500);
+/**
+ * Mapbox setup 
+ */
+mapboxgl.accessToken = 'pk.eyJ1IjoiamlubnljaG81MDMiLCJhIjoiY2o2am16cnA5MDhxMTMycGR0MXRhaDZxNiJ9.lNDt1qFFi4V7zUin8Jj1LQ';
+var map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/light-v10',
+  zoom: 1,
+  center: [-122.447303, 37.753574]
 });
+
+/**
+ * Mapbox + D3
+ */
+var canvas = map.getCanvasContainer();
+// overlay d3 on the map
+var svg = d3.select(canvas)
+    .append("svg")
+
+map.on('load', function() {
+  d3.json('./data/clusters-1.geojson', (err, data) => {
+    drawMap(data);
+  });
+});
+
+// project geojson coordinate to the map's current state
+function project(d) {
+  return map.project(new mapboxgl.LngLat(+d[0], +d[1]));
+}
+
+/**
+ * D3
+ */
+var circles;
+function drawMap(data) {
+  console.log("drawMap");
+  circles = svg.selectAll("circle")
+              .data(data.features)
+              .enter()
+              .append("circle")
+              .attr("r", 16)
+  update();
+  map.on("viewreset", update);
+  map.on("move", update);
+  map.on("moveend", update);
+} 
+
+function update() {
+  console.log("update");
+  circles
+    .attr("cx", function(d) { return project(d.geometry.coordinates).x })
+    .attr("cy", function(d) { return project(d.geometry.coordinates).y });
+}
