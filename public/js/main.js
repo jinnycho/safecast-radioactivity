64a479b4a6d93cebc4aec1259c2f1b9a32be76f8
Jinny Cho <45345735+jinnycho@users.noreply.github.com>
2019-07-21 15:04:58 -0400

    Have a better code style (#26)
    * Install eslinter
    
    * Add initial linting
    
    * replace double quote to single
    
    * Update indentation
    
    * Add pretest
    
    * Finish updating eslint in main.js
    
    * Add .eslintrc.json
    
    * Update app.js
    
    * Avoid using var
    
    * Use arrow functions

--- public/js/main.js
+++ public/js/main.js
@@ -1,8 +1,3 @@
-'use strict';
-const width = 700;
-const height = 580;
-let view = "map";
-
 /*
  * This file is mainly used to
  * 1. render the map
@@ -13,27 +8,33 @@ let view = "map";
  * Mapbox setup
  */
 mapboxgl.accessToken = 'pk.eyJ1IjoiamlubnljaG81MDMiLCJhIjoiY2o2am16cnA5MDhxMTMycGR0MXRhaDZxNiJ9.lNDt1qFFi4V7zUin8Jj1LQ';
-var map = new mapboxgl.Map({
+const map = new mapboxgl.Map({
   container: 'map',
   style: 'mapbox://styles/mapbox/light-v10',
   zoom: 1,
-  center: [-122.447303, 37.753574]
+  center: [-122.447303, 37.753574],
 });
 
 /**
  * Mapbox + D3
  */
-var canvas = map.getCanvasContainer();
+const canvas = map.getCanvasContainer();
 // overlay d3 on the map
-var svg = d3.select(canvas)
-    .append("svg")
+const svg = d3.select(canvas)
+  .append('svg');
 
-map.on('load', function() {
+map.on('load', (result, err) => {
+  if (err) throw (new Error('Error loading a map'));
   d3.json('./data/clusters-1.geojson', (err, data1) => {
+    if (err) throw (new Error('Error reading clusters-1.geojson'));
     d3.json('./data/clusters-2.geojson', (err, data2) => {
+      if (err) throw (new Error('Error reading clusters-2.geojson'));
       d3.json('./data/clusters-3.geojson', (err, data3) => {
+        if (err) throw (new Error('Error reading clusters-3.geojson'));
         d3.json('./data/clusters-4.geojson', (err, data4) => {
+          if (err) throw (new Error('Error reading clusters-4.geojson'));
           d3.json('./data/clusters-5.geojson', (err, data5) => {
+            if (err) throw (new Error('Error reading clusters-5.geojson'));
             drawMap(data1, data2, data3, data4, data5);
           });
         });
@@ -43,61 +44,61 @@ map.on('load', function() {
 });
 
 // project geojson coordinate to the map's current state
-function project(d) {
+const project = (d) => {
   return map.project(new mapboxgl.LngLat(+d[0], +d[1]));
 }
 
 /**
  * D3
  */
-function circleColor(d, i) {
-  if (d.properties.value < 10) return "#9ACD32";
-  if (d.properties.value >= 10 && d.properties.value < 30) return "#FFD700";
-  if (d.properties.value >= 30 && d.properties.value < 50) return "#FFA500";
-  if (d.properties.value >= 50 && d.properties.value < 100) return "#DC143C";
+const circleColor = (d, i) => {
+  if (d.properties.value < 10) return '#9ACD32';
+  if (d.properties.value >= 10 && d.properties.value < 30) return '#FFD700';
+  if (d.properties.value >= 30 && d.properties.value < 50) return '#FFA500';
+  if (d.properties.value >= 50 && d.properties.value < 100) return '#DC143C';
   // warning level
-  return "#283747"
+  return '#283747';
 }
 
-var tooltip = d3.select("body")
-                .append("div")
-                .attr("class", "tooltip")
-                .style("opacity", 0);
+const tooltip = d3.select('body')
+  .append('div')
+  .attr('class', 'tooltip')
+  .style('opacity', 0);
 
-var circles;
-function drawMap(data1, data2, data3, data4, data5) {
-  circles = svg.selectAll("circle")
-              .data(data1.features)
-              .data(data2.features)
-              .data(data3.features)
-              .data(data4.features)
-              .data(data5.features)
-              .enter()
-              .append("circle")
-              .attr("r", 7)
-              .attr("fill", (d, i) => circleColor(d, i))
-              .on("mouseover", (d) => {
-                tooltip.transition()
-                       .duration(200)
-                       .style("opacity", .9);
-                tooltip.html(d.properties.value.toFixed(2))
-                       .style("left", (d3.event.pageX) + "px")
-                       .style("top", (d3.event.pageY - 10) + "px");
-              })
-              .on("mouseout", (d) => {
-                tooltip.transition()
-                       .duration(500)
-                       .style("opacity", 0);
-              });
+let circles;
+const drawMap = (data1, data2, data3, data4, data5) => {
+  circles = svg.selectAll('circle')
+    .data(data1.features)
+    .data(data2.features)
+    .data(data3.features)
+    .data(data4.features)
+    .data(data5.features)
+    .enter()
+    .append('circle')
+    .attr('r', 7)
+    .attr('fill', (d, i) => circleColor(d, i))
+    .on('mouseover', (d) => {
+      tooltip.transition()
+        .duration(200)
+        .style('opacity', 0.9);
+      tooltip.html(d.properties.value.toFixed(2))
+        .style('left', (d3.event.pageX) + 'px')
+        .style('top', (d3.event.pageY - 10) + 'px');
+    })
+    .on('mouseout', (d) => {
+      tooltip.transition()
+        .duration(500)
+        .style('opacity', 0);
+    });
 
   update();
-  map.on("viewreset", update);
-  map.on("move", update);
-  map.on("moveend", update);
+  map.on('viewreset', update);
+  map.on('move', update);
+  map.on('moveend', update);
 }
 
-function update() {
+const update = () => {
   circles
-    .attr("cx", function(d) { return project(d.geometry.coordinates).x })
-    .attr("cy", function(d) { return project(d.geometry.coordinates).y });
+    .attr('cx', (d) => { return project(d.geometry.coordinates).x; })
+    .attr('cy', (d) => { return project(d.geometry.coordinates).y; });
 }
